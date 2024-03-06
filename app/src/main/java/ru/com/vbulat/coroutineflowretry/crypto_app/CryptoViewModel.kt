eb2905aa2aa21fa9be7eaa4c8ae555f4d9bff7c0
VB <Vladimir.Bulat@gmail.com>
2024-03-06 15:21:14 +0300

    Lesson 04 (Flow Lifecycle Operators)

--- app/src/main/java/ru/com/vbulat/coroutineflowretry/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/ru/com/vbulat/coroutineflowretry/crypto_app/CryptoViewModel.kt
@@ -4,31 +4,34 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
     private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
+    val state : LiveData<State> = _state
 
     init {
         loadData()
     }
 
     private fun loadData() {
-        viewModelScope.launch {
-            while (true) {
+        repository.getCurrencyList()
+            .onStart {
                 val currentState = _state.value
                 if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
                     _state.value = State.Loading
                 }
-                val currencyList = repository.getCurrencyList()
-                _state.value = State.Content(currencyList = currencyList)
-                delay(3000)
             }
-        }
+            .filter { it.isNotEmpty() }
+            .onEach {
+                _state.value = State.Content(currencyList = it)
+            }
+            .launchIn(viewModelScope)
     }
 }
